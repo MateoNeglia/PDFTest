0bd0722dd3f5678f66cdfd39e4fcf7569f9c3566
Your Name <mateo.neglia@gmail.com>
2020-04-17 19:12:02 -0300

    botones agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { TablaComponent } from './tabla/tabla.component';
+import { PdfMakeWrapper } from 'pdfmake-wrapper';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 
+
+PdfMakeWrapper.setFonts(pdfFonts);
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as jsPDF from 'jspdf';
+import { PdfMakeWrapper, Table, Img } from 'pdfmake-wrapper';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import html2canvas from 'html2canvas';
+import { async } from '@angular/core/testing';
+
 
 @Component({
   selector: 'app-tabla',
@@ -10,40 +15,40 @@ export class TablaComponent implements OnInit {
 
   constructor() { }
   column: string;
-  headers = ["ID", "Cliente", "Matriz"];
-
+  headers = ['ID', 'Cliente', 'Matriz'];
+  options = '';
   rows = [
     {
-      "ID" : "1",
-      "Cliente" : "HSBC",
-      "Matriz" : "GO123",
+      'ID' : '1',
+      'Cliente' : 'HSBC',
+      'Matriz' : 'GO123',
 
     },
     {
-      "ID" : "2",
-      "Cliente" : "Banco Galicia",
-      "Matriz" : "UV321",
+      'ID' : '2',
+      'Cliente' : 'Banco Galicia',
+      'Matriz' : 'UV321',
     },
     {
-      "ID" : "3",
-      "Cliente" : "Banco Valve",
-      "Matriz" : "HL3",
+      'ID' : '3',
+      'Cliente' : 'Banco Valve',
+      'Matriz' : 'HL3',
     },
     {
-      "ID" : "4",
-      "Cliente" : "Banco Santander",
-      "Matriz" : "GO123",
+      'ID' : '4',
+      'Cliente' : 'Banco Santander',
+      'Matriz' : 'GO123',
 
     },
     {
-      "ID" : "5",
-      "Cliente" : "Banco Provincia",
-      "Matriz" : "UV321",
+      'ID' : '5',
+      'Cliente' : 'Banco Provincia',
+      'Matriz' : 'UV321',
     },
     {
-      "ID" : "6",
-      "Cliente" : "Banco FF",
-      "Matriz" : "FFVII",
+      'ID' : '6',
+      'Cliente' : 'Banco FF',
+      'Matriz' : 'FFVII',
     }
   ];
 
@@ -53,17 +58,54 @@ export class TablaComponent implements OnInit {
   // matriz: ['GO123', 'UV321', 'HL3'];
 
   imprimirLista(){
+
+
     // se crea una nueva constante del elemento jspdf
     const doc = new jsPDF();
     // se crean la propiadad from HTML y save
     // fromHTML requiere recibir tres parametros: el elemento que vamos a convertir a pdf, so posicion en el eje x y su posicion en el eje Y
     doc.fromHTML(document.getElementById('frm1'), 10 , 10);
-    doc.save('Tabla de Usuarios');
+    // doc.window.open('new.pdf', '_blank', this.options);
+    const blob = new Blob([doc],  { type: jsPDF.toString() });
+    const url = window.URL.createObjectURL(blob);
+    doc.save();
+
+  }
+  generarPdf(){
+
+    PdfMakeWrapper.setFonts(pdfFonts);
+
+    const pdf = new PdfMakeWrapper();
+
+    pdf.add(
+      new Table([
+        ['ID', 'CLiente', 'Matriz'],
+        ['1', 'Banco Provincia', 'GO123'],
+        ['2', 'Banco ICBC', 'GO123'],
+        ['3', 'Banco Frances', 'GO123'],
+        ['4', 'Banco Galicia', 'GO123']
+      ]).end
+      );
+
+    // esto en conjunto genera un blob en una pestaña nueva con el pdf
+    pdf.create().open();
 
   }
 
-  ngOnInit(): void {
+  generateImage(){
+
+    html2canvas(document.getElementById('frm1')).then(canvas => {
+      document.body.appendChild(canvas);
+  })
 
   }
+
+  onPrintInvoice(){
+      window.print();
+
+  }
+
+
+  ngOnInit(): void {}
 
 }
